24160515601ba2430a042cae2679c07bbed5091e
dingchuanxin <chuanxin.ding@compiler-dev.com>
2020-08-19 18:20:35 +0800

    Pre-compiled toolchains for Linux-x86 platform used for llvm-rv

---
+++ usr/lib/gcc/riscv32-unknown-elf/10.0.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/hardisk/ding/work/test/riscv-gnu-toolchain/riscv-gcc/configure --target=riscv32-unknown-elf --prefix=/hardisk/ding/work/test/usr --disable-shared --disable-threads --enable-languages=c,c++ --with-system-zlib --enable-tls --with-newlib --with-sysroot=/hardisk/ding/work/test/usr/riscv32-unknown-elf --with-native-system-header-dir=/include --disable-libmudflap --disable-libssp --disable-libquadmath --disable-libgomp --disable-nls --disable-tm-clone-registry --src=.././riscv-gcc --disable-multilib --with-abi=ilp32 --with-arch=rv32imafc --with-tune=rocket 'CFLAGS_FOR_TARGET=-Os   -mcmodel=medlow' 'CXXFLAGS_FOR_TARGET=-Os   -mcmodel=medlow'";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "ilp32" }, { "arch", "rv32imafc" }, { "tune", "rocket" } };
